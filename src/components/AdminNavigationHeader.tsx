a9ecf9c6f843d0155236fad707a022ca1b02c69b
ZoutigeWolf <guuskamphuis@gmail.com>
2023-10-03 10:39:14 +0200

    update navigation

--- src/components/AdminNavigationHeader.tsx
+++ src/components/AdminNavigationHeader.tsx
@@ -4,9 +4,7 @@ const AdminNavigationHeader = () => {
     return (<>
         <div className={"navigation-header"}>
             <img src={logo} alt={"Logo"}/>
-            <a href={"machines"}>MACHINES</a>
             <a href={"issues"}>ISSUES</a>
-            <a href={"customers"}>CUSTOMERS</a>
             <a href={"logout"}>LOG OUT</a>
         </div>
     </>);
