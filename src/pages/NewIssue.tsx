44a5389709ada41b43907a37e951120b96c1deee
Levi <vandaalenlevi@gmail.com>
2023-10-23 20:45:09 +0200

    Updated NewIssue frontend

--- src/pages/NewIssue.tsx
+++ src/pages/NewIssue.tsx
@@ -3,15 +3,19 @@ import NavigationHeader from "../components/NavigationHeader.tsx";
 import Dropdown from "react-dropdown";
 import 'react-dropdown/style.css';
 import InputFile from "../components/InputFile.tsx";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const NewIssue = () => {
     useEffect(() => {
         document.title = "New Issue";
     }, []);
 
-    const [, setDepartment] = useState("");
-    const [, setTitle] = useState("");
+    const navigate = useNavigate();
+    const [department, setDepartment] = useState("");
+    const [title, setTitle] = useState("");
+    const [occurrence, setOccurence] = useState("");
+    const [expectation, setExpectation] = useState("");
+    const [tried, setTried] = useState("");
 
     const [media, setMedia] = useState([]);
 
@@ -32,8 +36,31 @@ const NewIssue = () => {
     }
 
     const onSubmit = () => {
-        axios.post("http://localhost:7183/")
+        let missingMessage = "";
+        if (department == "")
+            missingMessage += "Department; "
+        if (title == "")
+            missingMessage += "Title; "
+        if (occurrence == "")
+            missingMessage += "What Happened?; "
+        if (expectation == "")
+            missingMessage += "Expectations; "
+        if (tried == "")
+            missingMessage += "What did you try?;"
+
+
+        if (missingMessage != "")  {
+            alert("Missing: " + missingMessage);
+        } else {
+            handleSubmit();
+        }
     }
+
+    const handleSubmit = () => {
+        alert("Issue has been issued");
+        navigate("/");
+    }
+
     const onImageUpload = () => {
         if (imageInput.current === null) {
             return;
@@ -60,19 +87,24 @@ const NewIssue = () => {
     return (<>
         <NavigationHeader/>
         <div className={"page-content new-issue"}>
+            <h1>Create Issue</h1>
             <div className={"section"}>
                 <Dropdown options={departments} onChange={e => setDepartment(e.value)} placeholder={"Department..."}/>
                 <input type={"text"} onChange={e => setTitle(e.target.value)} placeholder={"Title..."}/>
             </div>
-            <p>What Happened?</p>
-            <textarea rows={10}/>
+            <div className={"observation-fields"}>
+                <p>What Happened?</p>
+                <textarea rows={10} placeholder={"..."}
+                    onChange={e => setOccurence(e.target.value)}/>
 
-            <p>Expectations</p>
-            <textarea rows={10}/>
+                <p>Expectations</p>
+                <textarea rows={10} placeholder={"..."}
+                    onChange={e => setExpectation(e.target.value)}/>
 
-            <p>What did you try?</p>
-            <textarea rows={10}/>
-
+                <p>What did you try?</p>
+                <textarea rows={10} placeholder={"..."}
+                    onChange={e => setTried(e.target.value)}/>
+            </div>
             <p>Files</p>
             <input type={"file"} accept={".png,.jpeg,.jpg,.mp4"} ref={imageInput} onChange={onImageUpload} style={{ display: "none" }}/>
             <div className={"files-list"}>
