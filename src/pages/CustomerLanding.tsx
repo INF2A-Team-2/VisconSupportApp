e4d12fb847630611b3d010f954029538fb256931
ZoutigeWolf <guuskamphuis@gmail.com>
2023-10-30 14:14:06 +0100

    update landing pages

--- src/pages/CustomerLanding.tsx
+++ src/pages/CustomerLanding.tsx
@@ -1,11 +1,15 @@
 import NavigationHeader from "../components/NavigationHeader.tsx";
 import WideButton from "../components/WideButton.tsx";
+import useAuth from "../api/auth.ts";
+import {AccountType} from "../models.ts";
 
 const CustomerLanding = () => {
+    const user = useAuth([AccountType.User]);
+
     return (<>
         <NavigationHeader/>
         <div className={"page-content"}>
-            <h1>Welcome Customer</h1>
+            <h1>Welcome {user?.username}</h1>
             <p>Recent Issues:</p>
             <WideButton title={"Test"} target={"test"}/>
         </div>
