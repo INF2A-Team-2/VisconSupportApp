97a2bb089eb8f1fa61cdb09b73dfdd486ce5b4d8
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-02 16:15:04 +0100

    hooks for all endpoints

--- src/api/machines.ts
+++ src/api/machines.ts
@@ -1,15 +1,25 @@
 import axios from "axios";
-import { Machine} from "../models";
-import { useEffect, useState } from "react";
+import {AccountType, Machine} from "../models";
+import {useCallback, useEffect, useState} from "react";
 import { RequestConfig, SERVER_URL } from "./auth";
 
-export function useMachines() {
+export function useMachines(userId: number | null = null) {
     const [machines, setMachines] = useState<Array<Machine>>([]);
-    useEffect(() => {
-        axios.get(SERVER_URL + "/api/machines", RequestConfig())
+
+    const fetchData = useCallback(() => {
+        axios.get(SERVER_URL + (userId === null ? "/api/machines" : `/api/${userId}/machines`), RequestConfig())
             .then(response => {
                 setMachines(response.data);
             });
-    });
-    return machines;
+    }, [userId]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return {machines, setMachines, refreshMachines: fetchData};
+}
+
+export function editUserMachines(userId: number, data: Array<Machine>) {
+    return axios.put(SERVER_URL + `/api/users/${userId}/machines`, data.map(m => m.id), RequestConfig());
 }
