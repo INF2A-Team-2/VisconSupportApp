20ceb6b19cad11acc06615a2ffc3892e29993852
Luuk <luuk.volkering@gmail.com>
2023-11-11 23:23:07 +0100

    Made upload request work but still need to make fancy

--- src/pages/CSVUploadPage.tsx
+++ src/pages/CSVUploadPage.tsx
@@ -1,7 +1,9 @@
 import {ChangeEvent, useState} from "react";
 import NavigationHeader from "../components/NavigationHeader.tsx";
-import useAuth from "../api/auth.ts";
+import useAuth, {SERVER_URL} from "../api/auth.ts";
 import {AccountType} from "../models.ts";
+import axios from "axios";
+import {toast} from "react-hot-toast";
 
 const CSVUploadPage = () => {
     useAuth([AccountType.Admin]);
@@ -18,6 +20,13 @@ const CSVUploadPage = () => {
         // You can implement the file upload logic here
         if (selectedFile) {
             console.log(`Uploading file: ${selectedFile.name}`);
+            let fd = new FormData();
+            fd.append('FormFile', selectedFile);
+            axios.post(SERVER_URL + "/api/import", fd,
+                {headers: {'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${sessionStorage.getItem("token")}`}})
+                .then(res => {
+                    if(res.status === 200){toast.success("File uploaded successfully!")
+                    }});
             // Add your file upload logic here
         } else {
             console.log('No file selected');
