7385d8be40874875c05482d64f7e195c164dd43e
patchzyy <64382339+patchzyy@users.noreply.github.com>
2023-11-16 13:44:59 +0100

    kleine text + meer ruimte

--- src/pages/renderIssueDetails.tsx
+++ src/pages/renderIssueDetails.tsx
@@ -13,14 +13,12 @@ export const RenderIssueDetails = ({ issue, isExpanded }: { issue: Issue; isExpa
 
     const detailClass = isExpanded ? "issue-details expanded" : "issue-details hidden";
 
-    // Function to shorten text and append ellipsis
     const shortenText = (text: string) => {
         return text.length > maxTextLength ? text.substring(0, maxTextLength) + "..." : text;
     };
 
-    // Function to calculate and set maximum text length based on window width
     const updateTextLength = () => {
-        setMaxTextLength(window.innerWidth / 20)
+        setMaxTextLength(window.innerWidth / 18)
     };
 
     useEffect(() => {
@@ -33,9 +31,9 @@ export const RenderIssueDetails = ({ issue, isExpanded }: { issue: Issue; isExpa
 
     return (
         <div className={detailClass}>
-            <p>What did happen: {shortenText(issue.actual)}</p>
-            <p>Expectation: {shortenText(issue.expected)}</p>
-            <p>What was tried: {shortenText(issue.tried)}</p>
+            <p>What did happen: <span className="small-text">{shortenText(issue.actual)}</span></p>
+            <p>Expectation: <span className="small-text">{shortenText(issue.expected)}</span></p>
+            <p>What was tried: <span className="small-text">{shortenText(issue.tried)}</span></p>
             <button onClick={(e) => navigateToIssue(issue.id, e)}>View Entire Issue</button>
         </div>
     );
