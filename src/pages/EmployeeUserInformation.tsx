267d72899039aa02402da8796aa3d710b502937e
patchzyy <64382339+patchzyy@users.noreply.github.com>
2023-11-18 16:28:04 +0100

    update

--- src/pages/EmployeeUserInformation.tsx
+++ src/pages/EmployeeUserInformation.tsx
@@ -6,6 +6,7 @@ import { useIssues } from "../api/issues.ts";
 import NavigationHeader from "../components/NavigationHeader.tsx";
 import { RenderIssueDetails } from "../components/RenderIssueDetails.tsx";
 import TableList from "../components/TableList.tsx";
+import {useNavigate} from "react-router-dom";
 
 
 const EmployeeUserInformation = () => {
@@ -13,22 +14,33 @@ const EmployeeUserInformation = () => {
     const { machines } = useUserMachines({ userId: parseInt(userId, 10) });
     const { issues } = useIssues({ userId: parseInt(userId, 10) });
     const { user } = useUser({ userId: parseInt(userId, 10) });
-
+    const navigate = useNavigate();
     const machineColumns = ["ID", "Name"];
     const issueColumns = ["ID", "Headline", "Machine", "Status"];
 
     const machineData = machines.map(machine => [machine.id, machine.name]);
-    const issueData = issues.map(issue => [issue.id, issue.headline, issue.machineId, "(Not implemented))"]);
+    const issueData = issues.map(issue => [issue.id, issue.headline, issue.machineId, "(Not implemented)"]);
     
 
     return (
         <>
             <NavigationHeader />
             <div className={"page-content"}>
-                <h1>User's Issues</h1>
-                <TableList columns={issueColumns} data={issueData} />
+                <h1> Gebruikersnaam Info (todo)</h1>
+
                 <h2>User's Machines</h2>
-                <TableList columns={machineColumns} data={machineData} />
+                <TableList columns={machineColumns} data={machineData}
+                 />
+
+                <h2>User's Issues</h2>
+                <TableList columns={issueColumns} data={issueData}
+                buttons={[
+                    {
+                        text: <i className="fa-solid fa-arrow-right"></i>,
+                        callback: (issueId) => navigate(`/issue/${issueId}`)
+                    }
+                ]}
+                />
             </div>
         </>
     );
