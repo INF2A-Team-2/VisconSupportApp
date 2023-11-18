f2adf8495843cea11257e2b11cec35dce1da4006
patchzyy <64382339+patchzyy@users.noreply.github.com>
2023-11-18 15:38:31 +0100

    users Issues + machines are visible now

--- src/pages/EmployeeUserInformation.tsx
+++ src/pages/EmployeeUserInformation.tsx
@@ -1,33 +1,34 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useUserMachines } from "../api/machines.ts";
-import { useUser } from "../api/users.ts"; // Import useUser hook
+import { useUser } from "../api/users.ts";
+import { useIssues } from "../api/issues.ts";
 import NavigationHeader from "../components/NavigationHeader.tsx";
+import { RenderIssueDetails } from "../components/RenderIssueDetails.tsx";
+import TableList from "../components/TableList.tsx";
+
 
 const EmployeeUserInformation = () => {
     const { userId } = useParams();
     const { machines } = useUserMachines({ userId: parseInt(userId, 10) });
+    const { issues } = useIssues({ userId: parseInt(userId, 10) });
     const { user } = useUser({ userId: parseInt(userId, 10) });
+
+    const machineColumns = ["ID", "Name"];
+    const issueColumns = ["ID", "Headline", "Machine", "Status"];
+
+    const machineData = machines.map(machine => [machine.id, machine.name]);
+    const issueData = issues.map(issue => [issue.id, issue.headline, issue.machineId, "(Not implemented))"]);
+    
 
     return (
         <>
             <NavigationHeader />
             <div className={"page-content"}>
-                <h1>User Information</h1>
-                {user && (
-                    <div>
-                        <h2>{user.username}</h2>
-                        <p>ID: {user.id}</p>
-                        <p>Phone Number: {user.phoneNumber}</p>
-                        <p>Unit: {user.unit}</p>
-                        <p>Type: {user.type}</p>
-                    </div>
-                )}
+                <h1>User's Issues</h1>
+                <TableList columns={issueColumns} data={issueData} />
                 <h2>User's Machines</h2>
-                <ul>
-                    {machines.map(machine => (
-                        <li key={machine.id}>{machine.name}</li>
-                    ))}
-                </ul>
+                <TableList columns={machineColumns} data={machineData} />
             </div>
         </>
     );
