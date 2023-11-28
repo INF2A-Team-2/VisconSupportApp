e506ce4682b7a5b27c5520c709496bbb88b629e8
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-28 02:16:08 +0100

    Get app name and version

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+      __APP_NAME__: JSON.stringify(process.env.npm_package_name),
+  }
 });
