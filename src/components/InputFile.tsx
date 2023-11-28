394ab508dffd29dd021472f20e79a9a39c7f12c6
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-28 03:09:43 +0100

    fix videos on new issue and open image in fullscreen

--- src/components/InputFile.tsx
+++ src/components/InputFile.tsx
@@ -1,7 +1,11 @@
-const InputFile = ({data, deleteCallback}) => {
+const InputFile = ({data, mimeType, deleteCallback}) => {
     return (<>
         <div className={"input-file"}>
-            <img src={data}/>
+            {mimeType.split("/")[0] === "image"
+            ? <img src={data}></img>
+            : <video controls={true}>
+                <source src={data}/>
+            </video>}
             <button onClick={deleteCallback}><i className="fa-solid fa-x"></i></button>
         </div>
     </>);
