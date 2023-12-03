f326ea3a18a7c415837f9af8f9d415578552c9cd
patchzyy <64382339+patchzyy@users.noreply.github.com>
2023-12-03 10:38:51 +0100

    rebase

--- src/pages/UserEditorPage.tsx
+++ src/pages/UserEditorPage.tsx
@@ -1,19 +1,63 @@
-import { useState } from "react";
 import NavigationHeader from "../components/NavigationHeader.tsx";
-import Dropdown from "react-dropdown";
-import { WideButton } from "../components/WideButton.tsx";
-import { useNavigate } from "react-router-dom";
-import { Machine, Issue } from "../models.ts";
-import 'react-dropdown/style.css';
-import '../index.css';
-import { useMachines } from "../api/machines.ts";
-import { useIssues } from "../api/issues.ts";
-import { RenderIssueDetails } from "../components/RenderIssueDetails.tsx";
+import useAuth from "../api/auth.ts";
+import { AccountType } from "../models.ts";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { editUser, useUser } from "../api/users.ts";
 
-const UserEditPage = () => {
+const UserEditorPage = () => {
+    const userId = parseInt(useParams().userId);
 
-    <NavigationHeader/>
-    return <h1>Hello World</h1>
-}
+    useAuth([AccountType.User]);
 
-export default UserEditPage;
+    const { user: editedUser, setUser: setEditedUser } = useUser({ userId: userId });
+
+    const handlePhoneNumberChange = (phoneNumber: string) => {
+        setEditedUser({
+            ...editedUser,
+            phoneNumber: phoneNumber,
+        });
+    };
+
+    const submitData = () => {
+        // Ensure that the user type is included in the data being submitted
+        const userDataToUpdate = {
+            phoneNumber: editedUser.phoneNumber,
+            type: editedUser.type
+        };
+    
+        const promise = editUser({
+            userId: userId,
+            data: userDataToUpdate,
+        });
+    
+        toast.promise(promise, {
+            loading: "Loading...",
+            success: "Phone number updated",
+            error: "Failed to update phone number"
+        });
+    };
+    
+
+    return <>
+        <NavigationHeader />
+        <div className={"page-content user-editor"}>
+            <div className={"page-header"}>
+                <h1>Edit Profile</h1>
+                <button onClick={submitData}>Apply changes</button>
+            </div>
+            {editedUser && <>
+                <p>Username</p>
+                <input type={"text"} value={editedUser.username ?? ""} readOnly/>
+                <p>Account Type</p>
+                <input type={"text"} value={AccountType[editedUser.type] ?? ""} readOnly/>
+                <p>Phone number</p>
+                <input type={"tel"} autoComplete={"off"} value={editedUser.phoneNumber ?? ""} onChange={(e) => handlePhoneNumberChange(e.target.value)}/>
+                {/* Other non-editable fields can be added here */}
+            </>}
+        </div>
+    </>;
+};
+
+export default UserEditorPage;
