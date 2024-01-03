400abd5966e608862a8aa000c6555d94a71105f0
patchzyy <64382339+patchzyy@users.noreply.github.com>
2024-01-03 13:33:33 +0100

    dark and light mode :)

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -37,7 +37,7 @@ const NavigationHeader = () => {
 
     let headerComponent = <></>;
 
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
 
     useEffect(() => {
         const savedTheme = localStorage.getItem('theme') || 'light';
@@ -50,9 +50,35 @@ const NavigationHeader = () => {
         const newTheme = theme === 'light' ? 'dark' : 'light';
         setTheme(newTheme);
         localStorage.setItem('theme', newTheme);
-        document.body.className = newTheme;
+        applyTheme(newTheme);
     }
 
+
+    const applyTheme = (theme) => {
+        const root = document.documentElement;
+    
+        if (theme === 'light') {
+            root.style.setProperty('--accent', 'var(--light-accent)');
+            root.style.setProperty('--accent-2', 'var(--light-accent-2)');
+            root.style.setProperty('--accent-3', 'var(--light-accent-3)');
+            root.style.setProperty('--background', 'var(--light-background)');
+            root.style.setProperty('--background-2', 'var(--light-background-2)');
+            root.style.setProperty('--background-3', 'var(--light-background-3)');
+            root.style.setProperty('--text', 'var(--light-text)');
+            root.style.setProperty('--text-hover', 'var(--light-text-hover)');
+        }
+        else if (theme === 'dark') {
+            root.style.setProperty('--accent', 'var(--dark-accent)');
+            root.style.setProperty('--accent-2', 'var(--dark-accent-2)');
+            root.style.setProperty('--accent-3', 'var(--dark-accent-3)');
+            root.style.setProperty('--background', 'var(--dark-background)');
+            root.style.setProperty('--background-2', 'var(--dark-background-2)');
+            root.style.setProperty('--background-3', 'var(--dark-background-3)');
+            root.style.setProperty('--text', 'var(--dark-text)');
+            root.style.setProperty('--text-hover', 'var(--dark-text-hover)');
+        }
+    };
+    
     
 
     if (user !== null) {
